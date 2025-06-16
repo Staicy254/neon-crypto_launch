277b26df26b01e7edd7a35cc92c47c295d639a37
Jean Staicy Auma Otambo <jeanstaicy21@gmail.com>
2025-06-17 00:35:42 +0300

    vite-update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/neon-crypto_launch/", 
+  base: "/", 
   server: {
     host: "::",
     port: 8080,
